63733d803376b64b60f2a580b95561a31fde63f7
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2025-07-15 21:43:36 +0530

    updated

--- contracts/src/Panagram.sol
+++ contracts/src/Panagram.sol
@@ -2,14 +2,20 @@
 pragma solidity ^0.8.13;
 
 import {ERC1155} from  "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol"
 import {IVerifier} from "./Verifier.sol";
 
-contract panagram is ERC1155 {
+contract panagram is ERC1155,Ownerable {
     IVerifier public immutable verifier;
 
-   constructor(IVerifier _verifier) ERC1155("ipfs://QmcxEUp6MCy2GthBVHomcojs5jBABmr5U39v8XRWeHGKB5/{id}.json"){
+   event Panagram_VerifierUpdated(IVerifier verifier);
+   constructor(IVerifier _verifier) ERC1155("ipfs://QmcxEUp6MCy2GthBVHomcojs5jBABmr5U39v8XRWeHGKB5/{id}.json") Owner(msg.sender){
         verifier = _verifier;
     }
     
+    function setVerifier(IVerifier _verifier)  external onlyOwner {
+        verifier = _verifier;
+        emit Panagram_VerifierUpdated(_verifier)
+    }
    
 }
